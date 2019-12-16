c61309b05b1f2b326b7f3afa8bddec951284da05
25shwetak <25shwetakshasha.com>
2019-12-16 17:17:05 +0530

    task 2 completed

---
+++ assignment2.rb
@@ -0,0 +1,11 @@
+puts "Enter grade"
+x = gets.to_i
+if x.between?(1,5)
+	puts "elementary"
+elsif x.between?(6,8)
+	puts "middle school"
+elsif x.between?(9,12)
+	puts "high school"
+else
+	puts "college"
+end
