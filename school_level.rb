6f9ad9228f5d8574eecdc346be0589653ab0d4a3
25shwetak <25shwetakshasha.com>
2019-12-22 22:47:45 +0530

    program to show level of school of students based on their grades

--- school_level.rb
+++ school_level.rb
@@ -1,11 +1,13 @@
-puts "Enter grade"
+# frozen_string_literal: true
+
+puts 'Enter grade'
 x = gets.to_i
-if x.between?(1,5)
-  puts "elementary"
-elsif x.between?(6,8)
-  puts "middle school"
-elsif x.between?(9,12)
-  puts "high school"
+if x.between?(1, 5)
+  puts 'elementary'
+elsif x.between?(6, 8)
+  puts 'middle school'
+elsif x.between?(9, 12)
+  puts 'high school'
 else
-  puts "college"
+  puts 'college'
 end
