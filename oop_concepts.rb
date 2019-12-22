27d211c40b7ed0b7289201d8c29433dda39e8ea8
25shwetak <25shwetakshasha.com>
2019-12-22 22:42:27 +0530

    program showing oop concepts

--- oop_concepts.rb
+++ oop_concepts.rb
@@ -1,33 +1,36 @@
-class Employee				#class defination and creation
-    attr_accessor :x, :name          #in place of getter and setter
+# frozen_string_literal: true
 
-    @@class_val =2				#class variable
-    def initialize(name,x)			#parameterized constructor
-        puts "Object created"
-        @name = name
-        @x = x
-    end
+class Employee # class defination and creation
+  attr_accessor :x, :name # in place of getter and setter
 
-    public						#access specifiers  (public, private, protected)
-    def policy             				#instance method
-        p 'policy method called'
-        Employee.salary				#class method calling
-    end
+  @@class_val = 2 # class variable
+  def initialize(name, x) # parameterized constructor
+    puts 'Object created'
+    @name = name
+    @x = x
+  end
 
-    def self.salary					#class method
-        p 'salary method called'
-    end
+  public # access specifiers  (public, private, protected)
 
-    # def name                   			getter
-    # 	@name
-    # end
+  def policy # instance method
+    p 'policy method called'
+    Employee.salary # class method calling
+  end
 
-    # def set_name(name)          			setter
-    # 	@name = name
-    # end
+  def self.salary # class method
+    p 'salary method called'
+  end
+
+  # def name                         getter
+  #   @name
+  # end
+
+  # def set_name(name)                setter
+  #   @name = name
+  # end
 end
 
-obj = Employee.new('kumari',100000) 		#(object creation) object is an instance of class
+obj = Employee.new('kumari', 100_000) # (object creation) object is an instance of class
 
 # puts obj.name
 
@@ -41,8 +44,7 @@ obj = Employee.new('kumari',100000) 		#(object creation) object is an instance o
 # obj.x = 50000
 # puts obj.x
 
-# puts obj.instance_variables			   to display instance variables in a class
-# puts Employee.class_variables			   to display class variables in a class
+# puts obj.instance_variables         to display instance variables in a class
+# puts Employee.class_variables         to display class variables in a class
 
-obj.policy		 				#instance method calling
-
+obj.policy # instance method calling
