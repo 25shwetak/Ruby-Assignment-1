e62f9e8fdff79a01a9ee488a0dd89591775edf47
25shwetak <25shwetakshasha.com>
2019-12-22 22:45:24 +0530

    ruby program for regex patterns generation

--- regex.rb
+++ regex.rb
@@ -1,94 +1,94 @@
-print "Enter mobile number"
-n=gets
+# frozen_string_literal: true
+
+print 'Enter mobile number'
+n = gets
 
 if n.match(/\A[1-9][0-9]{9}\Z/)
-  print "A Mobile number"
+  print 'A Mobile number'
 else
-  print "Not a mobile number"
+  print 'Not a mobile number'
 end
 
-puts " "
-puts " "
+puts ' '
+puts ' '
 
-print "Enter email address"
-n=gets
+print 'Enter email address'
+n = gets
 
 if n.match(/\A[a-zA-Z0-9]+[.|_]*[a-zA-Z0-9]+[@][a-zA-Z]+[.][a-z]{3}\Z/)
-  print "A valid email address"
+  print 'A valid email address'
 else
-  print "Not a valid email address"
+  print 'Not a valid email address'
 end
 
-puts " "
-puts " "
+puts ' '
+puts ' '
 
-print "Enter a Name"
-n=gets
+print 'Enter a Name'
+n = gets
 
 if n.match(/\A[a-zA-Z]+\s[a-zA-Z]+\Z/)
-  print "A valid Name"
+  print 'A valid Name'
 else
-  print "Not a valid Name"
+  print 'Not a valid Name'
 end
 
-puts " "
-puts " "
+puts ' '
+puts ' '
 
-print "Enter gender"
-n=gets
+print 'Enter gender'
+n = gets
 
 if n.match(/[M]|[m]|[F]|[f]/)
-  print "Valid"
+  print 'Valid'
 elsif match(/\A[F]+\Z/)
-  print "Invalid"
+  print 'Invalid'
 end
 
-print "Enter a Amount"
-n=gets
+print 'Enter a Amount'
+n = gets
 
 if n.match(/\A[0-9]*[.]?[0-9]+\Z/)
-  print "A valid Amount"
+  print 'A valid Amount'
 else
-  print "Not a valid Amount"
+  print 'Not a valid Amount'
 end
 
-puts " "
-puts " "
+puts ' '
+puts ' '
 
-print "Enter IFSC Code"
-n=gets
+print 'Enter IFSC Code'
+n = gets
 
 if n.match(/\A[A-Z]+{4}[0][0-9]+{6}\Z/)
-  print "A valid IFSC Code"
+  print 'A valid IFSC Code'
 else
-  print "Not a valid IFSC Code"
+  print 'Not a valid IFSC Code'
 end
 
-puts " "
-puts " "
+puts ' '
+puts ' '
 
-
-print "Enter Pan Card Number"
-n=gets
+print 'Enter Pan Card Number'
+n = gets
 
 if n.match(/\A[A-Z]+{5}[0-9]+{4}[A-Z]\Z/)
-  print "A valid Pan Card Number"
+  print 'A valid Pan Card Number'
 else
-  print "Not a valid Pan Card number"
+  print 'Not a valid Pan Card number'
 end
 
-puts " "
-puts " "
+puts ' '
+puts ' '
 
-
-print "Enter GST Number"
-n=gets
+print 'Enter GST Number'
+n = gets
 
 if n.match(/\A[0-9]+{2}[A-Z]+{5}[0-9]+{4}[A-Z][0-9][Z][0-9]\Z/)
-  print "A valid GST Number"
+  print 'A valid GST Number'
 else
-  print "Not a valid GST Number"
+  print 'Not a valid GST Number'
 end
 
-puts " "
-puts " "
+puts ' '
+puts ' '
