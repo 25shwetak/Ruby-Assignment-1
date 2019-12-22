33617e3cab0fa56973396e8cb2a57f0aa6b04c9f
25shwetak <25shwetakshasha.com>
2019-12-22 22:41:36 +0530

    ruby program for hangman game

--- hangman.rb
+++ hangman.rb
@@ -1,53 +1,51 @@
-puts "What is your name ?"
+# frozen_string_literal: true
+
+puts 'What is your name ?'
 name = gets
 
 puts "Good Luck #{name}"
 
-words = ['rainbow', 'computer', 'science', 'programming', 'python', 'mathematics', 'player', 'condition', 'reverse', 'water', 'board', 'geeks']
+words = %w[rainbow computer science programming python mathematics player condition reverse water board geeks]
 
 word = words.sample
-puts "Guess the characters"
+puts 'Guess the characters'
 guesses = ''
 
 turns = 7
 
-while turns > 0
+while turns.positive?
   failed = 0
   word.each_char do |char|
     if guesses.include?(char)
       print "#{char} "
     else
-      print "_ "
-      failed +=1
+      print '_ '
+      failed += 1
     end
   end
 
-  puts " "
-  
-  if failed == 0
-    puts "You win"
+  puts ' '
+
+  if failed.zero?
+    puts 'You win'
     puts "The word is : #{word}"
     break
   end
 
-  puts "Guess a character :"
+  puts 'Guess a character :'
   guess = gets
   guesses += guess
-  i=0
-  while i<word.length
-    if guess[0]==word[i]
-      break
-    end
-    i+=1
+  i = 0
+  while i < word.length
+    break if guess[0] == word[i]
+
+    i += 1
   end
 
-  if i==word.length
-    turns -= 1
-    puts "Wrong\n"
-    puts "You have #{turns} more guesses\n"
-    if turns == 0
-      puts "You loose"
-    end
-  end
+  next unless i == word.length
+
+  turns -= 1
+  puts "Wrong\n"
+  puts "You have #{turns} more guesses\n"
+  puts 'You loose' if turns.zero?
 end
-
