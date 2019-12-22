6e6accac9628faae43eef31d4d84c73b15e7b67b
25shwetak <25shwetakshasha.com>
2019-12-22 22:43:23 +0530

    hangman code updated with methods and blocks

--- updated_hangman.rb
+++ updated_hangman.rb
@@ -1,55 +1,53 @@
-def hangman
+# frozen_string_literal: true
 
-  puts "What is your name ?"
+def hangman
+  puts 'What is your name ?'
   name = gets
 
   puts "Good Luck #{name}"
 
-  words = ['rainbow', 'computer', 'science', 'programming', 'python', 'mathematics', 'player', 'condition', 'reverse', 'water', 'board', 'geeks']
+  words = %w[rainbow computer science programming python mathematics player condition reverse water board geeks]
 
   word = words.sample
-  puts "Guess the characters"
+  puts 'Guess the characters'
   guesses = ''
 
   turns = 7
 
-  while turns > 0
+  while turns.positive?
     failed = 0
     word.each_char do |char|
       if guesses.include?(char)
         print "#{char} "
       else
-        print "_ "
-        failed +=1
+        print '_ '
+        failed += 1
       end
     end
-    puts " "
+    puts ' '
 
-    if failed == 0
-      puts "You win"
+    if failed.zero?
+      puts 'You win'
       puts "The word is : #{word}"
       break
     end
 
-    puts "Guess a character :"
+    puts 'Guess a character :'
     guess = gets
     guesses += guess
-    i=0
-    while i<word.length
-      if guess[0]==word[i]
-        break
-      end
-      i+=1
+    i = 0
+    while i < word.length
+      break if guess[0] == word[i]
+
+      i += 1
     end
 
-    if i==word.length
-      turns -= 1
-      puts "Wrong\n"
-      puts "You have #{turns} more guesses\n"
-      if turns == 0
-        puts "You loose"
-      end
-    end
+    next unless i == word.length
+
+    turns -= 1
+    puts "Wrong\n"
+    puts "You have #{turns} more guesses\n"
+    puts 'You loose' if turns.zero?
   end
 end
 
@@ -57,9 +55,9 @@ flag = true
 
 while flag
   hangman
-  p "Do you want to play more ? (y/n)"
+  p 'Do you want to play more ? (y/n)'
   ans = gets.chomp
-  if ans == "n"
+  if ans == 'n'
     flag = false
     break
   end
