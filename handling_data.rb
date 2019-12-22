e941d6741a36af6db348af841045f7dd7dfcd56f
25shwetak <25shwetakshasha.com>
2019-12-22 18:41:08 +0530

    changes in the program has been resolved

--- handling_data.rb
+++ handling_data.rb
@@ -1,29 +1,40 @@
-module CheckApplicability
+# frozen_string_literal: true
 
+# module
+module CheckApplicability
   def loan_applicability?(population, gdp, states, state)
-    return true if population > 200000 && gdp >= 20000 && states >= 5 && state == "developing"
+    if population > 200_000 && gdp >= 20_000 && states >= 5 && state == 'developing'
+      return true
+    end
+
     false
   end
 
   def seat_applicability?(population, gdp, states, state)
-    return true if population > 200000 && gdp >= 10000 && states >= 8 && state == "developed"
+    if population > 200_000 && gdp >= 10_000 && states >= 8 && state == 'developed'
+      return true
+    end
+
     false
   end
 
   def win_war?(population, states, army_strength)
-    return true if population > 50000 && states >= 10 && army_strength >= 20000
+    if population > 50_000 && states >= 10 && army_strength >= 20_000
+      return true
+    end
+
     false
   end
-
 end
 
+# class
 class Country
   extend CheckApplicability
 
   attr_accessor :population, :gdp, :states, :army_strength, :state_of_country, :country_name, :country_code
   @@code = 0
 
-  def initialize (population, gdp, states, army_strength, state, country_name)
+  def initialize(population, gdp, states, army_strength, state, country_name)
     @population = population
     @gdp = gdp
     @states = states
@@ -37,37 +48,34 @@ class Country
     @@code += 1
     @country_code = @@code
   end
-
 end
 
 countries = []
 
 def create_object
-
-  puts "Enter Country name :"
+  puts 'Enter Country name :'
   country_name = gets.chomp
-  puts "Enter Population of the Country :"
+  puts 'Enter Population of the Country :'
   population = gets.to_i
-  puts "Enter GDP :"
+  puts 'Enter GDP :'
   gdp = gets.to_i
-  puts "Enter number of States in the country :"
+  puts 'Enter number of States in the country :'
   states = gets.to_i
-  puts "Enter Strength of Army :"
+  puts 'Enter Strength of Army :'
   army_strength = gets.to_i
-  puts "What is the state of country (developing/developed)? :"
+  puts 'What is the state of country (developing/developed)? :'
   state = gets.chomp
   country_object = Country.new(population, gdp, states, army_strength, state, country_name)
   puts "The country code of #{country_name} is #{country_object.code}"
-  return country_object
-
+  country_object
 end
 
 loop do
-  puts "********MENU********"
-  puts "1. Enter details of Country"
-  puts "2. Show details of the Country"
-  puts "3. Check applicability of the country"
-  puts "4. Exit"
+  puts '********MENU********'
+  puts '1. Enter details of Country'
+  puts '2. Show details of the Country'
+  puts '3. Check applicability of the country'
+  puts '4. Exit'
 
   option = gets.to_i
   case option
@@ -75,11 +83,11 @@ loop do
     countries.push(create_object)
 
   when 2
-    puts "Enter code of country you want to see details"
+    puts 'Enter code of country you want to see details'
     code = gets.to_i
 
     if code <= 0 || code > countries.size
-      puts "Sorry! No country with this code exist. "
+      puts 'Sorry! No country with this code exist. '
     else
       code -= 1
       puts "Country name : #{countries[code].country_name} (code : #{countries[code].country_code})"
@@ -91,18 +99,18 @@ loop do
     end
 
   when 3
-    puts "Enter code of country you want to check ?"
+    puts 'Enter code of country you want to check ?'
     code = gets.to_i
 
     if code <= 0 || code > countries.size
-      puts "Sorry! No country with this code exist. "
+      puts 'Sorry! No country with this code exist. '
     else
       code -= 1
       loop do
         puts "1. Check whether #{countries[code].country_name} is applicable for getting loan ?"
         puts "2. Check whether #{countries[code].country_name} is applicable for having seat in UN Security council ?"
         puts "3. Check whether #{countries[code].country_name} can win the war or not ?"
-        puts "4. Exit"
+        puts '4. Exit'
 
         option1 = gets.to_i
         case option1
@@ -131,16 +139,11 @@ loop do
           end
 
         end
-        if option1 == 4
-          break
-        end
-
+        break if option1 == 4
       end
 
     end
 
   end
-  if option == 4
-    break
-  end
+  break if option == 4
 end
